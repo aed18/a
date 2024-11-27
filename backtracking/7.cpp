c460b99e7c7d57b2f66ebb044468ed2a2f7e9cdc
aed18 <jean.marck.salazar.123@gmail.com>
2024-11-27 17:58:24 -0500

    Update 7.cpp

--- backtracking/7.cpp
+++ backtracking/7.cpp
@@ -2,37 +2,36 @@
 #include <vector>
 using namespace std;
 
-void dfs(int node, vector<vector<int>>& graph, vector<bool>& visited) {
-    visited[node] = true;
-    for (int neighbor : graph[node]) {
-        if (!visited[neighbor]) {
-            dfs(neighbor, graph, visited);
+void dfs(int nodo, vector<vector<int>>& grafo, vector<bool>& visitado) {
+    visitado[nodo] = true;
+    for (int vecino : grafo[nodo]) {
+        if (!visitado[vecino]) {
+            dfs(vecino, grafo, visitado);
         }
     }
 }
 
-bool isConnected(vector<vector<int>>& graph) {
-    int n = graph.size();
-    vector<bool> visited(n, false);
-
-    // Iniciar DFS desde el nodo 0
-    dfs(0, graph, visited);
-
-    // Comprobar si todos los nodos fueron visitados
-    for (bool nodeVisited : visited) {
-        if (!nodeVisited) return false;
+bool esConexo(vector<vector<int>>& grafo, int n) {
+    vector<bool> visitado(n, false);
+    dfs(0, grafo, visitado);
+    for (bool v : visitado) {
+        if (!v) return false;
     }
     return true;
 }
 
 int main() {
-    vector<vector<int>> graph = {
+    vector<vector<int>> grafo = {
         {1, 2},
         {0, 3},
         {0, 3},
         {1, 2}
     };
-
-    cout << "¿El grafo es conexo?: " << (isConnected(graph) ? "Sí" : "No") << endl;
+    int n = grafo.size();
+    if (esConexo(grafo, n)) {
+        cout << "El grafo es conexo." << endl;
+    } else {
+        cout << "El grafo no es conexo." << endl;
+    }
     return 0;
 }
